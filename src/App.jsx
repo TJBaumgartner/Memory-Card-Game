2e5479d35c6bc248712d285b257317730f5df5c3
TJBaumgartner <tnorth6@gmail.com>
2023-08-13 12:44:23 -0700

    fixed styles import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //import { useState } from 'react'
-import './styles/app.css'
+import './styles/App.css'
 import Header from './components/Header.jsx'
 import Main from './components/Main'
 import Footer from './components/Footer'
